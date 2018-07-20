f8c516285137a8c8bdcad5d33cdf48d0c45ec9c0
AndroidPayal <payalagrawal2311@gmail.com>
2018-07-20 12:22:01 +0530

    updates 1st 19-7-18

--- app/src/main/java/payal/cluebix/www/ecommerce/DashboardFragment.java
+++ app/src/main/java/payal/cluebix/www/ecommerce/DashboardFragment.java
@@ -56,7 +56,7 @@ import payal.cluebix.www.ecommerce.R;
  * Created by speed on 11-Jun-18.
  */
 
-public class DashboardFragment extends Fragment implements Recycler_item_adapter.ClickListener, SearchView.OnQueryTextListener {
+public class DashboardFragment extends Fragment implements Recycler_item_adapter.ClickListener, SearchView.OnQueryTextListener, View.OnClickListener {
 
     private String Tag="Dashboard_screen";
     RecyclerView recyclerView;
@@ -65,6 +65,7 @@ public class DashboardFragment extends Fragment implements Recycler_item_adapter
     String url1= Base_url.Dashboard_url;
     String url2=Base_url.Add_prod_to_Cart;
     String url3=Base_url.My_cart_item_count;
+    String url4=Base_url.Load_more_url;
 
     ArrayList<String> Product_id_array=new ArrayList<>();
     ArrayList<String> P_id_array_of_cartItems=new ArrayList<>();
@@ -73,7 +74,7 @@ public class DashboardFragment extends Fragment implements Recycler_item_adapter
     ArrayList<data_dashboard> product_item;
 
     SessionManager session;
-    String Uid;String Uname,Umail,Udate1,Udate2,Umob;
+    String Uid;String Uname,Umail,Udate1,Udate2,Umob,Lastid="0";
     public static int count=0;
     View v;
 
@@ -85,6 +86,7 @@ public class DashboardFragment extends Fragment implements Recycler_item_adapter
     LinearLayout loader_linear;
     View v2;
     SearchView tool_search;
+    TextView load_more;
 
     public DashboardFragment() {
     }
@@ -128,6 +130,7 @@ public class DashboardFragment extends Fragment implements Recycler_item_adapter
         loader_linear=(LinearLayout)v.findViewById(R.id.loader);
         v2=(View)v.findViewById(R.id.view_for_margin);
         tool_search=(SearchView)getActivity().findViewById(R.id.search_edit);
+        load_more=(TextView)v.findViewById(R.id.load_more);
 
         tool_search.setIconifiedByDefault(false);
         tool_search.setSubmitButtonEnabled(true);
@@ -151,6 +154,7 @@ public class DashboardFragment extends Fragment implements Recycler_item_adapter
 //        adapter.setClickListener(this);
 //        recyclerView.setAdapter(adapter);
 
+        load_more.setOnClickListener(this);
         return v;
     }
 
@@ -227,6 +231,8 @@ public class DashboardFragment extends Fragment implements Recycler_item_adapter
                         if(P_id_array_of_cartItems.contains(product_id))
                             cart_disable=1;
                         Log.d(Tag,"cart disable value="+cart_disable+" name="+product_name);
+
+                        Lastid=product_id;
                         product_item.add(new data_dashboard(product_id, product_name,product_code
                                 , color, price, product_images, sample, manufacturing,qty, amount,cart_disable));
                     }
@@ -335,6 +341,71 @@ public class DashboardFragment extends Fragment implements Recycler_item_adapter
         adapter.getFilter().filter(newText);
 
         return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.load_more){
+
+            StringRequest stringRequest=new StringRequest(Request.Method.POST, url4+Lastid, new Response.Listener<String>(){
+                @Override
+                public void onResponse(String response) {//url1+Uid
+                    Log.d("dashboard_correct_res","response load more="+response);
+
+                    JSONObject post_data;
+                    try {
+                        JSONObject obj=new JSONObject(response);
+                        JSONArray jsonArray=obj.getJSONArray("products");
+                        for(int i=0;i<jsonArray.length();i++) {
+                            post_data = jsonArray.getJSONObject(i);
+/*
+{"id":"24","product_name":"this is test product","product_code":"896833","price":"345.00","retail_price":"700.00","color":"red","product_images":"79e,79f","sample":"0","unit":"ufndi","manufacturing":"0","qty":"4","amount":"5.00","percent":"%"},{
+* */
+                            String product_id = post_data.getString("id");
+                            String product_name = post_data.getString("product_name");
+                            String product_code= post_data.getString("product_code");
+                            String retail_price=post_data.getString("retail_price");
+                            String color = post_data.getString("color");
+                            String price = post_data.getString("price");
+                            String product_images = post_data.getString("product_images");
+                            String sample=post_data.getString("sample");
+                            String manufacturing=post_data.getString("manufacturing");
+                            String qty=post_data.getString("qty");
+                            String amount=post_data.getString("amount");
+                            String percent=post_data.getString("percent");
+
+
+                            Product_id_array.add(product_id);
+                            int cart_disable = 0;
+                            name_list.add(product_name);
+
+                            if(P_id_array_of_cartItems.contains(product_id))
+                                cart_disable=1;
+                            Log.d(Tag,"cart disable value="+cart_disable+" name="+product_name);
+
+                            Lastid=product_id;
+                            product_item.add(new data_dashboard(product_id, product_name,product_code
+                                    , color, price, product_images, sample, manufacturing,qty, amount,cart_disable));
+                        }
+
+                    adapter.notifyData(product_item);
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("dashboard_error_res",error+"");
+                    Toast.makeText(getActivity(), "Server Connection Failed!", Toast.LENGTH_SHORT).show();
+                }
+            });
+            RquestHandler.getInstance(getActivity()).addToRequestQueue(stringRequest);
+
+        }
     }
 
 
