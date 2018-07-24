63119fda603611dcf06eaee22e3e9cb05488e9ed
AndroidPayal <payalagrawal2311@gmail.com>
2018-07-24 12:35:28 +0530

    updates 3rd 24-7-18

--- app/src/main/java/payal/cluebix/www/ecommerce/Adapter/Quotation_item_adap.java
+++ app/src/main/java/payal/cluebix/www/ecommerce/Adapter/Quotation_item_adap.java
@@ -1,8 +1,11 @@
 package payal.cluebix.www.ecommerce.Adapter;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -31,7 +35,7 @@ public class Quotation_item_adap extends RecyclerView.Adapter<Quotation_item_ada
     @Override
     public Quotation_item_adap.ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        View view = inflater.inflate(R.layout.invoice_adapter_layout, null,false);
+        View view = inflater.inflate(R.layout.invoice_adapter_layout, null, false);
         ViewGroup.LayoutParams lp = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT);
         view.setLayoutParams(lp);
@@ -41,32 +45,47 @@ public class Quotation_item_adap extends RecyclerView.Adapter<Quotation_item_ada
 
     @Override
     public void onBindViewHolder(Quotation_item_adap.ProductViewHolder holder, int position) {
-        final quotation2 a= productList.get(position);
+        final quotation2 a = productList.get(position);
 
-        holder.invoice_sr_number.setText((position+1)+"");
+        holder.invoice_sr_number.setText((position + 1) + "");
 
 
         holder.q_name.setText(a.getProduct_name());
         //holder.q_brand.setText("Brand: "+a.getBrand());
-        holder.q_desc.setText("Description: "+a.getDescription());
+        holder.q_desc.setText("Description: " + a.getDescription());
         holder.q_qty.setText(a.getQty());
-        holder.invoice_price.setText("Product price:"+a.getPrice());
+        holder.invoice_price.setText("Product price:" + a.getPrice());
 
         if (a.getSample().equals("1")) {
             holder.invoice_sample.setText("Sample:" + a.getSample_price());
-            holder.q_prise.setText("Rs. "+(Float.parseFloat(a.getPrice())*Integer.parseInt(a.getQty())+
-                            Float.parseFloat(a.getSample_price())));
-        }
-        else {
+            holder.q_prise.setText("Rs. " + (Float.parseFloat(a.getPrice()) * Integer.parseInt(a.getQty()) +
+                    Float.parseFloat(a.getSample_price())));
+        } else {
             holder.invoice_sample.setText("");
-            holder.q_prise.setText("Rs. "+(Float.parseFloat(a.getPrice())*Integer.parseInt(a.getQty())));
+            holder.q_prise.setText("Rs. " + (Float.parseFloat(a.getPrice()) * Integer.parseInt(a.getQty())));
         }
-        Log.d("quotation_item_adap__","qty="+a.getQty()+"\nsrNo:"+(position+1));
+        Log.d("quotation_item_adap__", "qty=" + a.getQty() + "\nsrNo:" + (position + 1));
 
         holder.image_Call.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mCtx.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("tel://" + ""+a.getMobile())));
+                // mCtx.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("tel://" + ""+a.getMobile())));
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel://" + "" + a.getMobile()));
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                if (ActivityCompat.checkSelfPermission(mCtx, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    Toast.makeText(mCtx, "Call Permission not granted", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mCtx.startActivity(intent);
+
+
             }
         });
 
